57f62afe82d2bd0f5d1df4c79563c05eee7f1ecd
matthew quinn <matthewquinn.personal@gmail.com>
2017-08-07 14:19:06 -0400

    initial commit

---
+++ src/main/java/com/apprenticeshiptasks/ApprenticeshiptasksApplication.java
@@ -0,0 +1,12 @@
+package com.apprenticeshiptasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprenticeshiptasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprenticeshiptasksApplication.class, args);
+	}
+}
