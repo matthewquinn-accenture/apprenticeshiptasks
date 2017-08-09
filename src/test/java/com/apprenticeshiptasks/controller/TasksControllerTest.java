9a90121e81f45c963708f4155f66e0946f5a4d1a
matthew quinn <matthewquinn.personal@gmail.com>
2017-08-09 11:34:18 -0400

    modified test to be able to view tasks by id

--- src/test/java/com/apprenticeshiptasks/controller/TasksControllerTest.java
+++ src/test/java/com/apprenticeshiptasks/controller/TasksControllerTest.java
@@ -1,26 +1,30 @@
 package com.apprenticeshiptasks.controller;
 
+import com.apprenticeshiptasks.repository.TasksRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebMvcTest
+@SpringBootTest
 public class TasksControllerTest {
     @Autowired
     private WebApplicationContext wac;
 
     private MockMvc mockMvc;
+
+    @Autowired
+    private TasksRepository tasksRepositoryMock;
 
     @Before
     public void setUp() {
@@ -29,11 +33,11 @@ public class TasksControllerTest {
 
     @Test
     public void viewOneTask() throws Exception {
-        this.mockMvc.perform(get("/task"))
+        this.mockMvc.perform(get("/task/{id}",1))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("task"))
-                .andExpect(model().attribute("task", "apple"));
+                .andExpect(model().attribute("task", tasksRepositoryMock.findOne(1)));
     }
 
 }
