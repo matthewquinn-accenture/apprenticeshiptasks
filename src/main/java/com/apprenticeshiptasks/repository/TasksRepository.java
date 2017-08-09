59a5c29e7d3b51b9a543d67f40bcb969924796db
matthew quinn <matthewquinn.personal@gmail.com>
2017-08-09 11:33:56 -0400

    created in-memory database for testing purposes

--- src/main/java/com/apprenticeshiptasks/repository/TasksRepository.java
+++ src/main/java/com/apprenticeshiptasks/repository/TasksRepository.java
@@ -1,9 +1,25 @@
 package com.apprenticeshiptasks.repository;
 
 import com.apprenticeshiptasks.model.Tasks;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Repository
-public interface TasksRepository extends CrudRepository<Tasks, Long>{
+public class TasksRepository{
+    private static final List<Tasks> TASKS_LIST = Arrays.asList(
+      new Tasks(1, "Test1", "Test1 Description"),
+      new Tasks(2, "Test2", "Test2 Description"),
+      new Tasks(3, "Test3", "Test3 Description")
+    );
+
+    public Tasks findOne(long id) {
+        for(Tasks task: TASKS_LIST) {
+            if(id == task.getId()){
+                return task;
+            }
+        }
+        return null;
+    }
 }
