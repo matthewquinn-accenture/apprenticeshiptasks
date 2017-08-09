d4172cd1052a4b549cd0e200e86b9ab29aafaa70
matthew quinn <matthewquinn.personal@gmail.com>
2017-08-09 11:35:31 -0400

    modified task method to map URI to id

--- src/main/java/com/apprenticeshiptasks/controller/TasksController.java
+++ src/main/java/com/apprenticeshiptasks/controller/TasksController.java
@@ -1,15 +1,21 @@
 package com.apprenticeshiptasks.controller;
 
+import com.apprenticeshiptasks.repository.TasksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TasksController {
 
-    @RequestMapping("/task")
-    public String task(Model model) {
-        model.addAttribute("task", "apple");
+    @Autowired
+    private TasksRepository tasksRepository;
+
+    @RequestMapping("/task/{id}")
+    public String task(@PathVariable long id, Model model) {
+        model.addAttribute("task", tasksRepository.findOne(id));
         return "task";
     }
 
