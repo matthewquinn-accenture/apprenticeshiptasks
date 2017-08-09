81d9fa7637518bfd1c7d858e8f39c79378403e07
matthew quinn <matthewquinn.personal@gmail.com>
2017-08-09 11:32:47 -0400

    commented out TasksLoader to create in-memory DB

--- src/main/java/com/apprenticeshiptasks/config/TasksLoader.java
+++ src/main/java/com/apprenticeshiptasks/config/TasksLoader.java
@@ -1,38 +1,38 @@
-package com.apprenticeshiptasks.config;
-
-
-import com.apprenticeshiptasks.model.Tasks;
-import com.apprenticeshiptasks.repository.TasksRepository;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.stereotype.Component;
-
-@Component
-public class TasksLoader implements ApplicationListener<ContextRefreshedEvent> {
-
-    @Autowired
-    private TasksRepository tasksRepository;
-
-    private Logger log = Logger.getLogger(TasksLoader.class);
-
-    @Autowired
-    public void setTaskRepository(TasksRepository taskRepository) {
-        this.tasksRepository = tasksRepository;
-    }
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-
-        Tasks tasks = new Tasks();
-        tasks.setName("Refactor Code");
-        tasksRepository.save(tasks);
-
-        log.info("Saved Task" + tasks.getId());
-
-        Tasks tasks1 = new Tasks();
-        tasks1.setName("Test Spring Application");
-        tasksRepository.save(tasks1);
-    }
-}
+//package com.apprenticeshiptasks.config;
+//
+//
+//import com.apprenticeshiptasks.model.Tasks;
+//import com.apprenticeshiptasks.repository.TasksRepository;
+//import org.apache.log4j.Logger;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationListener;
+//import org.springframework.context.event.ContextRefreshedEvent;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class TasksLoader implements ApplicationListener<ContextRefreshedEvent> {
+//
+//    @Autowired
+//    private TasksRepository tasksRepository;
+//
+//    private Logger log = Logger.getLogger(TasksLoader.class);
+//
+//    @Autowired
+//    public void setTaskRepository(TasksRepository taskRepository) {
+//        this.tasksRepository = tasksRepository;
+//    }
+//
+//    @Override
+//    public void onApplicationEvent(ContextRefreshedEvent event) {
+//
+//        Tasks tasks = new Tasks();
+//        tasks.setName("Refactor Code");
+//        tasksRepository.save(tasks);
+//
+//        log.info("Saved Task" + tasks.getId());
+//
+//        Tasks tasks1 = new Tasks();
+//        tasks1.setName("Test Spring Application");
+//        tasksRepository.save(tasks1);
+//    }
+//}
