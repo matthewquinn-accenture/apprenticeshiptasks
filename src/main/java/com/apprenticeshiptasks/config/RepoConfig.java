2d1cb41ac4ad36af64ebc6616708b47b46ca4299
matthew quinn <matthewquinn.personal@gmail.com>
2017-08-09 11:32:25 -0400

    commented out RepoConfig

--- src/main/java/com/apprenticeshiptasks/config/RepoConfig.java
+++ src/main/java/com/apprenticeshiptasks/config/RepoConfig.java
@@ -1,15 +1,15 @@
-package com.apprenticeshiptasks.config;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@Configuration
-@EnableAutoConfiguration
-@EntityScan(basePackages = {"com.apprenticeshiptasks.model"})
-@EnableJpaRepositories(basePackages = {"com.apprenticeshiptasks.repository"})
-@EnableTransactionManagement
-public class RepoConfig {
-}
+//package com.apprenticeshiptasks.config;
+//
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//@Configuration
+//@EnableAutoConfiguration
+//@EntityScan(basePackages = {"com.apprenticeshiptasks.model"})
+//@EnableJpaRepositories(basePackages = {"com.apprenticeshiptasks.repository"})
+//@EnableTransactionManagement
+//public class RepoConfig {
+//}
