8b799c85bb6127da94e696434c62718975b704e7
matthew quinn <matthewquinn.personal@gmail.com>
2017-08-09 11:59:51 -0400

    wrote application code to pass tests

--- src/main/java/com/apprenticeshiptasks/controller/HomeController.java
+++ src/main/java/com/apprenticeshiptasks/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.apprenticeshiptasks.controller;
 
+import com.apprenticeshiptasks.repository.TasksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private TasksRepository tasksRepository;
 
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("taskslist", tasksRepository.findAll());
         return "home";
     }
 }
